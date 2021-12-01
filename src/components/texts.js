9750a6bc51a26c8065893523e376766396b27d58
clement <hecquet.c59@gmail.com>
2021-12-01 19:33:39 +0100

    Added texts styles

---
+++ src/components/texts.js
@@ -0,0 +1,103 @@
+import styled from "styled-components";
+
+export const Heading1 = styled.h1`
+font-family: RoadRadio;
+font-size: 34px;
+font-style: normal;
+font-weight: 700;
+line-height: 41px;
+letter-spacing: 0px;
+text-align: left;
+`;
+export const Heading2 = styled.h2`
+font-family: RoadRadio;
+font-size: 22px;
+font-style: normal;
+font-weight: 700;
+line-height: 28px;
+letter-spacing: 0px;
+text-align: left;
+`;
+export const Heading3 = styled.h3`
+font-family: RoadRadio;
+font-size: 17px;
+font-style: normal;
+font-weight: 700;
+line-height: 22px;
+letter-spacing: 0px;
+text-align: left;
+`;
+export const Heading4 = styled.h3`
+font-family: RoadRadio;
+font-size: 15px;
+font-style: normal;
+font-weight: 700;
+line-height: 20px;
+letter-spacing: 0px;
+text-align: left;
+`;
+
+export const TextButton1 = styled.div`
+font-family: RoadRadio;
+font-size: 17px;
+font-style: normal;
+font-weight: 700;
+line-height: 22px;
+letter-spacing: 0px;
+text-align: left;
+`;
+export const TextButton2 = styled.div`
+font-family: RoadRadio;
+font-size: 13px;
+font-style: normal;
+font-weight: 700;
+line-height: 18px;
+letter-spacing: 0px;
+text-align: left;
+`;
+export const TextCaption1 = styled.div`
+font-family: RoadRadio;
+font-size: 12px;
+font-style: normal;
+font-weight: 700;
+line-height: 16px;
+letter-spacing: 0px;
+text-align: left;
+`;
+export const TextCaption2 = styled.div`
+font-family: RoadRadio;
+font-size: 11px;
+font-style: normal;
+font-weight: 700;
+line-height: 13px;
+letter-spacing: 0px;
+text-align: left;
+`;
+
+export const Body1 = styled.div`
+font-family: RoadRadio;
+font-size: 15px;
+font-style: normal;
+font-weight: 400;
+line-height: 20px;
+letter-spacing: 0px;
+text-align: left;
+`;
+export const Body2 = styled.div`
+font-family: RoadRadio;
+font-size: 13px;
+font-style: normal;
+font-weight: 400;
+line-height: 18px;
+letter-spacing: 0px;
+text-align: left;
+`;
+export const SmallText = styled.div`
+font-family: RoadRadio;
+font-size: 11px;
+font-style: normal;
+font-weight: 400;
+line-height: 13px;
+letter-spacing: 0px;
+text-align: left;
+`;
