ec9006d0a56a9dfe8f5777efaa56d3d948a55be6
clement <hecquet.c59@gmail.com>
2021-12-02 21:55:17 +0100

    Removed useless files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import {WalletProvider, getChainOptions} from "@terra-money/wallet-provider";
 
 getChainOptions().then((chainOptions) => {
@@ -15,8 +14,3 @@ getChainOptions().then((chainOptions) => {
         document.getElementById('root')
     );
 });
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
