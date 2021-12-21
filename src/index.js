c9301f698c26d6e96e842fbb6584c43a5a9f2ce3
clement <hecquet.c59@gmail.com>
2021-12-21 19:39:02 +0100

    Added notistack

--- src/index.js
+++ src/index.js
@@ -6,15 +6,27 @@ import {WalletProvider, getChainOptions} from "@terra-money/wallet-provider";
 import {Provider} from "react-redux";
 import store from "./store/store";
 import {BrowserRouter} from 'react-router-dom';
+import Slide from "@mui/material/Slide";
+import {SnackbarProvider} from "notistack";
 
 getChainOptions().then((chainOptions) => {
     ReactDOM.render(
         <WalletProvider {...chainOptions}>
             <Provider store={store}>
                 <BrowserRouter>
-                    <React.StrictMode>
-                        <App/>
-                    </React.StrictMode>
+                    <SnackbarProvider
+                        maxSnack={3}
+                        anchorOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}
+                        autoHideDuration={3000}
+                        TransitionComponent={Slide}
+                    >
+                        <React.StrictMode>
+                            <App/>
+                        </React.StrictMode>
+                    </SnackbarProvider>
                 </BrowserRouter>
             </Provider>
         </WalletProvider>,
